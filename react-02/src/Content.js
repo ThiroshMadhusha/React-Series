989b0a94a927880a8e8948e6f521bf23e147290e
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-28 11:46:17 +0530

    add buttons for click events function

---
+++ react-02/src/Content.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+// Click Events Function
+
+const Content = () => {
+  const handleClick = () => {
+    console.log("You Clicked it");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked.`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target);
+  };
+  return (
+    <main>
+      <p>Hello World..!!</p>
+
+      <button onClick={handleClick}>Click It</button>
+      
+      {/* Anonimous button */}
+      <button onClick={() => handleClick2("Thirosh")}>Click It</button>
+      
+      <button onClick={(e)=>handleClick3(e)}>Click It</button>
+    </main>
+  );
+};
+
+export default Content;
