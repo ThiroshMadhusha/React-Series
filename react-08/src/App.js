c86c73ac90bbcfd6c2b771b8d5e5d25e1ab4c9ae
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-29 22:22:29 +0530

    Complete for hexavalue display

--- react-08/src/App.js
+++ react-08/src/App.js
@@ -6,14 +6,21 @@ import { useState } from "react";
 function App() {
   const [colorValue, setColorValue] = useState("");
   const [hexValue, setHexValue] = useState("");
+  const [isDarkText, setIsDarkText] = useState("");
 
   return (
     <div className="App">
-      <Square colorValue={colorValue} hexValue={hexValue} />
+      <Square
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}
+      />
       <Input
         colorValue={colorValue}
         setColorValue={setColorValue}
         setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
       />
     </div>
   );
