4e29a41c4896d4063aee1de777a514e16d832ec5
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-01 16:56:09 +0530

    Create react app

---
+++ react-12/src/pages/apiRequest.js
@@ -0,0 +1,13 @@
+// CReate all api
+const apiRequest = async (url = "", optionsObj = null, errMsg = null) => {
+  try {
+    const response = await fetch(url, optionsObj);
+    if (!response.ok) throw Error("Please reload the app");
+  } catch (err) {
+    errMsg = err.message;
+  } finally {
+    return errMsg;
+  }
+};
+
+export default apiRequest;
