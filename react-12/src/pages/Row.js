a494785952e45d57d600363823ebc87c40e6a920
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-02 05:15:14 +0530

    add table row data here

---
+++ react-12/src/pages/Row.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cell from "./Cell";
+
+const Row = ({ item }) => {
+  return (
+    <tr>
+      {Object.entries(item).map(([key, value]) => {
+        return <Cell key={key} cellData={JSON.stringify(value)} />;
+      })}
+    </tr>
+  );
+};
+
+export default Row;
