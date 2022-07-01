c69823213ee6021d41b4f871b89ee0cc9f250cf8
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-02 05:15:28 +0530

    Complete main table

---
+++ react-12/src/pages/Table.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Row from './Row'
+
+const Table = ({items}) => {
+  return (
+      <div className='table-container'>
+          <table>
+              <tbody>
+                  {items.map(item => (
+                      <Row key={item.id} item={item} />
+                  ))}
+              </tbody>
+          </table>
+          
+    </div>
+  )
+}
+
+export default Table
