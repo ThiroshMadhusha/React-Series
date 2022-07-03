99c90724bb88282a357e5f8e01b05c8923f2e158
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-03 08:36:42 +0530

    Add Post page components

--- react-13/src/App.js
+++ react-13/src/App.js
@@ -4,33 +4,65 @@
 // latest react router dom is changing index and app js both files.
 
 // import all the component we create in this project
-import Footer from "./pages/Footer";
-import Header from "./pages/Header";
-import Nav from "./pages/Nav";
-import Home from "./pages/Home";
-import Missing from "./pages/Missing";
-import About from "./pages/About";
-import NewPost from "./pages/NewPost";
-import PostPage from "./pages/PostPage";
+import Footer from "./pages/footer/Footer";
+import Header from "./pages/header/Header";
+import Nav from "./pages/navbar/Nav";
+import Home from "./pages/home/Home";
+import Missing from "./pages/missing/Missing";
+import About from "./pages/about/About";
+import NewPost from "./pages/newpost/NewPost";
+import PostPage from "./pages/postpage/PostPage";
 
 import { Route, Switch, useHistory } from "react-router-dom";
 
 import { useState, useEffect } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const handleDelete = (id => {
+    
+  })
+
   return (
     <div className="App">
-      <Header />
-      <Nav />
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home posts={posts} />
         </Route>
         <Route exact path="/post">
           <NewPost />
         </Route>
         <Route exact path="/post/:id">
-          <PostPage />
+          <PostPage posts={posts} handleDelete={handleDelete} />
         </Route>
         <Route path="/about" component={About} />
         <Route path="*" component={Missing} />
