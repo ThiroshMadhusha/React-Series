b2384eef705cffa1885203c3886c721900320c86
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-03 08:39:18 +0530

    Create Nav Bar

---
+++ react-13/src/pages/navbar/Nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+
+const Nav = ({ search, setSearch }) => {
+  return (
+    <nav className="Nav">
+      <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+        <label htmlForm="search">Search Posts</label>
+        <input
+          id="search"
+          type="text"
+          placeholder="Search Posts"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
