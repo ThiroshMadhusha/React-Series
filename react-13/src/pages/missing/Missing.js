1f539e9727fb00c94517fee616efaed69b8580c1
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-03 23:21:13 +0530

    add missing file

--- react-13/src/pages/missing/Missing.js
+++ react-13/src/pages/missing/Missing.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './missing.css'
+
+import { Link } from 'react-router-dom'
 
 const Missing = () => {
   return (
-    <main>
-      <h1>Missing</h1>
+    <main className='Missing'>
+      <h1>Page Not Found</h1>
+      <p>Well, That is Disapointing.</p>
+      <p>
+        <Link to='/'>Visit Our Homepage</Link>
+      </p>
     </main>
   )
 }
