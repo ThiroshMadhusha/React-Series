0f470cb2e58a4e340c788f902edd2dc6188049b0
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-04 23:50:49 +0530

    add handle submit component

--- react-14/src/pages/newpost/NewPost.js
+++ react-14/src/pages/newpost/NewPost.js
@@ -1,13 +1,35 @@
 import React from "react";
 import "./newpost.css";
+import { useContext, useState } from "react";
+import DataContext from "../../context/DataContext";
+import { useHistory } from "react-router-dom";
+import api from "../../api/posts";
+import { format } from "date-fns";
 
-const NewPost = ({
-  handleSubmit,
-  postBody,
-  postTitle,
-  setPostBody,
-  setPostTitle,
-}) => {
+
+const NewPost = () => {
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const { posts, setPosts } = useContext(DataContext);
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM ddd,yyy pp");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    try {
+      const response = await api.post("/posts", newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostBody("");
+      history.push("/");
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
+
   return (
     <main className="NewPost">
       <h1>New Post</h1>
