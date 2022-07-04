8094910e879f79999adf271058cebe03c75410ba
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-04 13:19:40 +0530

    Complete about section

--- react-13/src/pages/about/About.js
+++ react-13/src/pages/about/About.js
@@ -3,10 +3,16 @@ import './about.css'
 
 const About = () => {
   return (
-    <main>
+    <main className="About">
       <h1>About US</h1>
+      <p style={{ marginTop: "1rem" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+        consequatur expedita, assumenda similique non optio! Modi nesciunt
+        excepturi corrupti atque blanditiis quo nobis, non optio quae possimus
+        illum exercitationem ipsa!
+      </p>
     </main>
-  )
+  );
 }
 
 export default About
