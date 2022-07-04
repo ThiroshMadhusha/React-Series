6c9c7febd3c14e45acd8f8a3ba7ca903651f10c7
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-04 20:30:10 +0530

    Update header and edit post

--- react-14/src/pages/edit/EditPost.js
+++ react-14/src/pages/edit/EditPost.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import posts from "../../api/posts";
 
 const EditPost = ({
   posts,
--- react-14/src/pages/header/Header.js
+++ react-14/src/pages/header/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./header.css";
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
 
-const Header = ({title}) => {
+const Header = ({ title ,width}) => {
   return (
     <header className="Header">
       <h1>{title}</h1>
+      {width < 768 ? (
+        <FaMobileAlt />
+      ) : width < 992 ? (
+        <FaTabletAlt />
+      ) : (
+        <FaLaptop />
+      )}
     </header>
   );
 };
