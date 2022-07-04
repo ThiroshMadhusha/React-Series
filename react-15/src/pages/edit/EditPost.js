5377993aa94be69e79312aa1002aa296423c9cf5
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-04 22:11:10 +0530

    Create react app

---
+++ react-15/src/pages/edit/EditPost.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+
+const EditPost = ({
+  posts,
+  handleEdit,
+  editBody,
+  setEditBody,
+  setEditTitle,
+  editTitle,
+}) => {
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+
+  useEffect(() => {
+    if (post) {
+      setEditTitle(post.title);
+      setEditBody(post.body);
+    }
+  }, [post, setEditBody, setEditTitle]);
+
+  return (
+    <main className="NewPost">
+      {editTitle && (
+        <>
+          <h1>Update Post</h1>
+          <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="postTitle">Title</label>
+            <input
+              id="postTitle"
+              type="text"
+              required
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+            />
+            <label htmlFor="postBody">Post</label>
+            <textarea
+              id="postBody"
+              required
+              value={editBody}
+              onChange={(e) => setEditBody(e.target.value)}
+            />
+            <button
+              className="editButton"
+              type="submit"
+              onClick={() => handleEdit(post.id)}
+            >
+              Submit
+            </button>
+          </form>
+        </>
+      )}
+      {!editTitle && (
+        <>
+          <h2>Not D=Found Data</h2>
+          <p>
+            <Link to="/">Go To Main Page</Link>
+          </p>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default EditPost;
---
+++ react-15/src/pages/postpage/PostPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./postpage.css";
+import { useParams, Link } from "react-router-dom";
+
+const PostPage = ({ posts, handleDelete }) => {
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+  return (
+    <main className="PostPage">
+      <article className="post">
+        {post && (
+          <>
+            <h2>{post.title}</h2>
+            <p className="postDate">{post.body}</p>
+            <Link to={`/edit/${post.id}`}>
+              <button className="editButton">Edit Post</button>
+            </Link>
+            <button
+              className="deleteButton"
+              onClick={() => handleDelete(post.id)}
+            >
+              Delete Post
+            </button>
+          </>
+        )}
+        {!post && (
+          <>
+            <h2>Post Not Found ..!!</h2>
+            <p>Well, That is Disappointing..</p>
+            <p>
+              <Link to="/">Visit Home Page</Link>
+            </p>
+          </>
+        )}
+      </article>
+    </main>
+  );
+};
+
+export default PostPage;
