cf8f0fcad5e3504c55fa1e3ae5f8bc75fa417b8d
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-06 16:55:06 +0530

    Complete home section

--- react-16/src/pages/home/Home.js
+++ react-16/src/pages/home/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Feed from "../feed/Feed";
-import { useContext } from "react";
-import DataContext from "../../context/DataContext";
+import Feed from '../feed/Feed'
+import { useStoreState } from "easy-peasy";
 
-const Home = () => {
-  const { searchResults, fetchError, isLoading } = useContext(DataContext);
+const Home = ({ isLoading, fetchError }) => {
+  const searchResults = useStoreState((state) => state.searchResults);
 
   return (
     <main className="Home">
-      {isLoading && <p className="statusMsg">Loading Posts...</p>}
+      {isLoading && <p className="statusMsg">Loading posts...</p>}
       {!isLoading && fetchError && (
         <p className="statusMsg" style={{ color: "red" }}>
           {fetchError}
@@ -19,9 +18,10 @@ const Home = () => {
         (searchResults.length ? (
           <Feed posts={searchResults} />
         ) : (
-          <p className="statusMsg">No posts to Display..!</p>
+          <p className="statusMsg">No posts to display.</p>
         ))}
     </main>
   );
 };
+
 export default Home;
